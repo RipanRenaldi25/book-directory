5bb68aecaddb30da1789cc08a99eb70c50d923e8
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2023-04-16 15:22:09 +0700

    (#) Refactor code (clean code)

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,17 +1,29 @@
 import booksSchema from '../schema/books.js';
+import { checkInputValid } from '../utils/utils.js';
 
 export const getAllBook = async (req, res) => {
   try {
-    const books = await booksSchema.find();
-    if (!books.length) {
-      throw new Error('There is no Books');
+    const allBooks = await booksSchema.find();
+    if (!allBooks.length) {
+      throw new Error('There is no Books to Find, please add book first');
     }
-    res.status(200).json({ books });
-  } catch (e) {
-    res.status(404).send(e.message).end();
+    res.status(200).json({ allBooks });
+  } catch (error) {
+    console.error(`Error in getAllBook Controller: ${error.message}`);
+    res.status(404).send('Failed to Retrive all books. Please try again later');
   }
 };
 
-export const insertBook = async (payload) => {
-  //
+export const insertBook = async (req, res) => {
+  try {
+    const {
+      id, title, author, publishedYear, publisher, genres,
+    } = req.body;
+    const isInputValid = checkInputValid([id, title, author, publishedYear, publisher, genres]);
+    if (!isInputValid) {
+      throw new Error(isInputValid);
+    }
+  } catch (err) {
+
+  }
 };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,15 @@
-const splitBySymbol = (data, symbol) => {
-  const newData = data.split(symbol);
-  return newData.map((value) => value.trim());
+// clean
+export const trimStrings = (strings) => {
+  if (!Array.isArray(strings)) {
+    throw new Error('Input must be an array of strings');
+  }
+  return strings.map((string) => string.trim());
 };
-export default splitBySymbol;
+
+// clean
+export const checkInputValid = (inputsInArray) => {
+  if (!Array.isArray(inputsInArray)) {
+    throw new Error('input must be an array of strings');
+  }
+  return inputsInArray.every((input) => input !== undefined);
+};
