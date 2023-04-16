5a81c4be4e566d8357597c0b23479e562fb3eb71
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2023-04-16 20:48:54 +0700

    (+) Add Unit Test For Get All Books

---
+++ src/__tests__/getAllBook.test.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import BookModel from '../schema/books';
+import main from '../app';
+
+/**
+ * test case screnario
+ * Should Return status code 200
+ * Should Return list of all books
+ * Should return "Failed to Retrive all books. Please try again later" when no books finded
+ * Should Return 404 when no books finded
+ */
+
+describe('Get All Book Controller', () => {
+  let app;
+  beforeAll(async () => {
+    app = await main();
+  });
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+  beforeEach(async () => {
+    await BookModel.deleteMany({});
+  });
+  it('Should Return status code 200', async () => {
+    const newData = new BookModel({
+      _id: +new Date(), publishedYear: 2000, author: 'Me', title: 'Enders Game',
+    });
+    await newData.save();
+    const response = await request(app).get('/books');
+    expect(response.status).toBe(200);
+  });
+  it('Should return list of all books', async () => {
+    const newData = new BookModel({
+      _id: +new Date(), publishedYear: 2000, author: 'Me', title: 'Enders Game',
+    });
+    await newData.save();
+    const response = await request(app).get('/books');
+    expect(response.text).toBeDefined();
+    expect(response.body).toEqual(expect.any(Object));
+  });
+  it('Should return "Failed to Retrive all books. Please try again later" when no books finded', async () => {
+    const response = await request(app).get('/books');
+    expect(response.status).toBe(404);
+  });
+});
